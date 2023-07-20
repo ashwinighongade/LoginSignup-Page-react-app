837a54b2e9ac4815aeab18162af286f6d933b88b
ashwinighongade <ashwinighongade21@navgurukul.org>
2023-07-20 23:49:16 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route  path="/Login" element={<Login/>} />
+        <Route  path="/" element={<Login/>} />
         <Route  path="/SignUp" element={<SignUp/>} />
       </Routes>
     </Router>
